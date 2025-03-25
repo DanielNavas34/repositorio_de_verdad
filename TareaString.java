da315f590d263147b72feec7116a724f589dd8dc
Daniel Navas34 <danielnavas711900@digitechfp.com>
2025-03-25 19:51:35 +0100

    for

--- TareaString.java
+++ TareaString.java
@@ -2,5 +2,9 @@ public class TareaString {
     public static void main(String[] args) {
         System.out.println("Programa iniciado.");
         
+        for(int i=1;i>10;i++){
+            System.out.println(i);
+        }
+
     }
 }
