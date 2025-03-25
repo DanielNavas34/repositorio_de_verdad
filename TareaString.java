855031b56ab985489e19bb67e466c2950306601a
Daniel Navas34 <danielnavas711900@digitechfp.com>
2025-03-25 20:03:03 +0100

    kd

--- TareaString.java
+++ TareaString.java
@@ -4,11 +4,7 @@ public class TareaString {
     public static void main(String[] args) {
         System.out.println("Programa iniciado.");
         
-        for(int i=1;i>10;i++){
-            System.out.println(i);
-        }
 
-        Scanner scanner = new Scanner(System.in);
-
+        
     }
 }
