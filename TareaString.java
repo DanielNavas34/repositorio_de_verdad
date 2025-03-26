a756aff813306e2cf2312481076f63e96e2d9b96
Daniel Navas34 <danielnavas711900@digitechfp.com>
2025-03-26 10:51:52 +0100

    confirmacion

--- TareaString.java
+++ TareaString.java
@@ -5,7 +5,7 @@ public class TareaString {
         System.out.println("Programa iniciado.");
         
         String daniel;
-        Int num2;
+        
 
         System.out.println("Pablo, ya mismo estamos borrando cosas de verdad.");
         
